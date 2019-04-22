6d66e06a70bfce8660837f0cbaf83360dc52843a
Dragomir Dan <dan@dragomirweb.com>
2019-04-22 16:30:13 +0300

    add pagination to recipe results
    splice, event delegation and markup render

--- src/js/index.js
+++ src/js/index.js
@@ -23,18 +23,18 @@ const controlSearch = async () => {
     console.log(query)
     
     if (query) {
-        //2. New search obj and add to thate
+        //* 2. New search obj and add to thate
         state.search = new Search(query);
 
-        //3.prepare Ui for result
+        //* 3.prepare Ui for result
         searchView.clearInput();
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        //4. Search for recipes
+        //*4. Search for recipes
         await state.search.getResults();
 
-        //5. Render results to ui
+        //* 5. Render results to ui
         clearLoader();
         searchView.renderResult(state.search.result);
     }
@@ -44,4 +44,13 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
-const search = new Search('pizza');
+
+elements.searchResPages.addEventListener('click', (e) => {
+    const btn = e.target.closest('.btn-inline');
+    
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResult(state.search.result, goToPage);
+    }
+})
