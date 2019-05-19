e2e082fa24a265406338ea6530d769544af6004f
Dragomir Dan <dan@dragomirweb.com>
2019-05-19 16:44:35 +0300

    add controller to update servings

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -111,4 +111,15 @@ export default class Recipe {
         });
         this.ingredients = newIngredients;
     }
+
+    updateServings(type) {
+        // Update servings 
+        const newServings = type === 'dec' ? this.serveings - 1 : this.serveings + 1;
+
+        // Ingredients
+        this.ingredients.forEach(ing => {
+            ing.count *= (newServings / this.serveings);
+        })
+        this.serveings = newServings;
+    }
 }
