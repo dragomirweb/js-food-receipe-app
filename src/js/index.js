70f34fb08fa16a32eebd59f18bb94541d99d401b
Dragomir Dan <dan@dragomirweb.com>
2019-05-19 11:44:17 +0300

    render recipe to view
    -add markup to recipe view
    -add recipe to list elements
    -add kg and g using destructuring
    -add loader
    -fix typos

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 
@@ -63,9 +64,10 @@ const controlRecipe = async () => {
     // Get the Id from the url 
     const id = window.location.hash.replace('#', '');
 
-    console.log(id)
     if (id) {
         // Prepare the UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
         // Create new recipe object
         state.recipe = new Recipe(id);
@@ -79,8 +81,9 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             //Render the recipe
-            console.log(state.recipe);
-            
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
         } catch (err) {
             alert('Error processing recipe')
         }
@@ -88,4 +91,4 @@ const controlRecipe = async () => {
     }
 };
 
-['haschange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -10,7 +10,7 @@ export default class Recipe {
         try {
             const res = await axios(`https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`);
             this.title = res.data.recipe.title;
-            this.autor = res.data.recipe.publisher;
+            this.author = res.data.recipe.publisher;
             this.img = res.data.recipe.image_url;
             this.url = res.data.recipe.source_url;
             this.ingredients = res.data.recipe.ingredients;
@@ -52,9 +52,14 @@ export default class Recipe {
             'cup',
             'pound'
         ];
+        const units = [...unitsShort,
+            'kg',
+            'g'
+        ]
         const newIngredients = this.ingredients.map(el => {
             // 1) Uniform units
             let ingredient = el.toLowerCase();
+
             unitsLong.forEach((unit, i) => {
                 ingredient = ingredient.replace(unit, unitsShort[i]);
             });
@@ -64,7 +69,7 @@ export default class Recipe {
 
             // 3) Parse ingredients into count, unit and ingredient
             const arrIng = ingredient.split(' ');
-            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
             let objIng;
             if (unitIndex > -1) {
